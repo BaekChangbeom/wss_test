508d33bc59e588c0cbff7a41aa89aa64f70a066c
BaekChangbeom <baek.cb@volo-inc.jp>
2017-01-15 17:08:02 +0900

    Add

--- utils/message.js
+++ utils/message.js
@@ -5,9 +5,6 @@ var validation = new Validation();
 function ChatMessage() {};
 
 ChatMessage.prototype.generateMessage = function cb(from, text, timestamp, uid) {
-  if (!validation.isRealString(from) || !validation.isRealString(text)) {
-    return new CustomError(global.err.unauthorized, 'generateMessage error');
-  }
   return {
     from:from,
     text:text,
